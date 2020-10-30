39ac3b2ec44d930a8cdb972b0a6108a5bcb1748f
CruciformHawk7 <CruciformHawk7@users.noreply.github.com>
2020-10-30 03:16:24 +0000

    deploy: 057756dbd6e7a844b239a6fcca0e35d474a47f64

--- Models/Note.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Noot.Models {
-    public class Note {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public string Notes { get; set; }
-    }
-}
